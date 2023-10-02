5843633619f6d8e18323822abdabfe94aa629e4d
AlphomArt <menelik.wubneh@gmail.com>
2023-10-02 17:16:25 +0200

    weird

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,23 +1,50 @@
 #include "iostream"
 #include "fstream"
 
+static int origin_fopen(std::ifstream&	original, std::string file_name);
+static int copy_fopen(std::ofstream&	copy, std::string newfile_name);
+
 int main(int argc, char **argv)
 {
-	(void)argv;
 	if (argc != 4)
 	{
-		std::cout << "\033[31mUsage : ./<exec> <filename> <string1> <string2>\033[0m" << std::endl;
-		return (0);
+		std::cerr << "\033[31mUsage : ./<exec> <filename> <string1> <string2>\033[0m" << std::endl;
+		return (EXIT_FAILURE);
 	}
-	std::ifstream original(argv[1]);
-	if (original.is_open())
-	{
-		std::cout << argv[1] << " as bien été ouvert" << std::endl;
-	}
-	else
-	{
-		std::cout << argv[1] << " n'est pas ouvert" << std::endl;
-		return (0);
-	}
+
+	const std::string		file_name(argv[1]);
+	const std::string		newfile_name(file_name + ".replace");
+	std::ifstream	original(file_name);
+	std::ofstream 	copy(newfile_name);
+
+	if (origin_fopen(original, file_name))
+		return (EXIT_FAILURE);
+	if (copy_fopen(copy, newfile_name))
+		return (EXIT_FAILURE);
+	std::cout << "tout est ok !" << std::endl;
 	return (0);
+}
+
+static int origin_fopen(std::ifstream&	original, std::string file_name)
+{
+	original.open(file_name);
+	if (!original)
+	{
+		std::cerr << "\033[31mError : " + file_name + " can't be open !\033[0m" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	std::cout << file_name + " est ouvert" << std::endl;
+	return (EXIT_SUCCESS);
+}
+
+static int copy_fopen(std::ofstream&	copy, std::string newfile_name)
+{
+	copy.open(newfile_name);
+	if (!copy)
+	{
+		std::cerr << "\033[31mError : " + newfile_name + " can't be create/open !\033[0m" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	std::cout << newfile_name + " est ouvert" << std::endl;
+	return (EXIT_SUCCESS);
 }
