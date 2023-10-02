e1fd5a1d3f2a57395d351c1caecdb2dc3e253efc
AlphomArt <menelik.wubneh@gmail.com>
2023-10-02 17:35:01 +0200

    executable names's changed, and refactors some output

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,31 +1,41 @@
 #include "iostream"
 #include "fstream"
 
+static int	valid_input(int argc);
 static int origin_fopen(std::ifstream&	original, std::string file_name);
 static int copy_fopen(std::ofstream&	copy, std::string newfile_name);
 
 int main(int argc, char **argv)
+{
+	if (valid_input(argc))
+		return (EXIT_FAILURE);
+
+	const std::string	file_name(argv[1]);
+	const std::string	newfile_name(file_name + ".replace");
+	const std::string	to_change;
+	const std::string	substitute;
+
+	std::ifstream	original;
+	if (origin_fopen(original, file_name)) {
+		return (EXIT_FAILURE);
+	}
+	std::ofstream 	copy;
+	if (copy_fopen(copy, newfile_name))
+		return (EXIT_FAILURE);
+	return (0);
+}
+
+static int	valid_input(int argc)
 {
 	if (argc != 4)
 	{
 		std::cerr << "\033[31mUsage : ./<exec> <filename> <string1> <string2>\033[0m" << std::endl;
 		return (EXIT_FAILURE);
 	}
-
-	const std::string		file_name(argv[1]);
-	const std::string		newfile_name(file_name + ".replace");
-	std::ifstream	original;
-	std::ofstream 	copy;
-
-	if (origin_fopen(original, file_name))
-		return (EXIT_FAILURE);
-	if (copy_fopen(copy, newfile_name))
-		return (EXIT_FAILURE);
-	std::cout << "tout est ok !" << std::endl;
-	return (0);
+	return (EXIT_SUCCESS);
 }
 
-static int origin_fopen(std::ifstream&	original, std::string file_name)
+static int	origin_fopen(std::ifstream&	original, std::string file_name)
 {
 	original.open(file_name);
 	if (!original)
@@ -33,11 +43,11 @@ static int origin_fopen(std::ifstream&	original, std::string file_name)
 		std::cerr << "\033[31mError : " + file_name + " can't be open !\033[0m" << std::endl;
 		return (EXIT_FAILURE);
 	}
-	std::cout << file_name + " est ouvert" << std::endl;
+	std::cout << file_name + " is open" << std::endl;
 	return (EXIT_SUCCESS);
 }
 
-static int copy_fopen(std::ofstream&	copy, std::string newfile_name)
+static int	copy_fopen(std::ofstream&	copy, std::string newfile_name)
 {
 	copy.open(newfile_name);
 	if (!copy)
@@ -45,6 +55,6 @@ static int copy_fopen(std::ofstream&	copy, std::string newfile_name)
 		std::cerr << "\033[31mError : " + newfile_name + " can't be create/open !\033[0m" << std::endl;
 		return (EXIT_FAILURE);
 	}
-	std::cout << newfile_name + " est ouvert" << std::endl;
+	std::cout << newfile_name + " is open" << std::endl;
 	return (EXIT_SUCCESS);
 }
